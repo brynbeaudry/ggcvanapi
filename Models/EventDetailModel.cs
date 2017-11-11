3e3eee518ac1c731ccec17048b3d7c065eb496b2
Bryn Beaudry <babeaudry@gmail.com>
2017-11-11 02:19:22 -0800

    Added Guest Class

---
+++ Models/EventDetailModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ggcvan.Models
+{
+    public class EventDetailModel
+    {
+        
+        public List<ApplicationUser> Guests
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ Models/Guest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ggcvan.Models
+{
+    public class Guest
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Guest(ApplicationUser user)
+        {
+            this.Id = user.Id;
+            this.FirstName = user.FullName;
+            this.LastName = user.LastName;
+        }
+
+        //public String Bio { get; set; }
+
+        //public List<Event> CreatedEvents { get; set; }
+
+        //public List<EventGuest> JoinedEvents { get; set; }
+    }
+}
