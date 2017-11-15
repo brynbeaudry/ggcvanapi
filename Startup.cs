1f6fcbec54915243896c601a375d2172089cc621
Bryn Beaudry <babeaudry@gmail.com>
2017-11-14 21:58:39 -0800

    about to test new startup, no build errors

--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,7 @@ using Newtonsoft.Json.Linq;
 using Swashbuckle.AspNetCore.Swagger;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using AspNet.Security.OpenIdConnect.Primitives;
 
 namespace ggcvan
 {
@@ -33,17 +34,31 @@ namespace ggcvan
             //var sqlConnectionString = Configuration.GetConnectionString("DataAccessMySqlProvider");
             
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+                {
+                    options.UseMySql(Configuration.GetConnectionString("DefaultConnection"));
+                    options.UseOpenIddict();
+                }
+            );
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
              .AddEntityFrameworkStores<ApplicationDbContext>()
              .AddDefaultTokenProviders();
 
+             // Configure Identity to use the same JWT claims as OpenIddict instead
+            // of the legacy WS-Federation claims it uses by default (ClaimTypes),
+            // which saves you from doing the mapping in your authorization controller.
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.ClaimsIdentity.UserNameClaimType = OpenIdConnectConstants.Claims.Name;
+                options.ClaimsIdentity.UserIdClaimType = OpenIdConnectConstants.Claims.Subject;
+                options.ClaimsIdentity.RoleClaimType = OpenIdConnectConstants.Claims.Role;
+            });
+
             services.AddSingleton<IConfiguration>(Configuration);
-
-
-
-            services.AddAuthentication().AddGoogle(googleOptions =>
+            
+            // Register the OAuth2 validation handler.
+            services.AddAuthentication()
+            .AddGoogle(googleOptions =>
             {
                 var GoogleConfig = Configuration.GetSection("ExternalIdentities").GetSection("Google");
                 //Console.WriteLine(GoogleConfig["client_id"]);
@@ -51,13 +66,15 @@ namespace ggcvan
 
                 googleOptions.ClientId = GoogleConfig["client_id"];
                 googleOptions.ClientSecret = GoogleConfig["client_secret"];
-            }).AddFacebook(facebookOptions =>
+            })
+            .AddFacebook(facebookOptions =>
             {
                 var FacebookConfig = Configuration.GetSection("ExternalIdentities").GetSection("Facebook");
                 facebookOptions.AppId = FacebookConfig["app_id"];
                 facebookOptions.AppSecret = FacebookConfig["app_secret"];
             })
-            .AddCookie(cfg => cfg.SlidingExpiration = true)
+            .AddOAuthValidation();
+            /* .AddCookie(cfg => cfg.SlidingExpiration = true)
             .AddJwtBearer(cfg =>
               {
                   cfg.RequireHttpsMetadata = false;
@@ -70,10 +87,53 @@ namespace ggcvan
                       IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Tokens:Key"]))
                   };
 
-              });
+              }); */
 
-            // Add application services.
-            services.AddTransient<IEmailSender, EmailSender>();
+            // Register the OpenIddict services.
+            services.AddOpenIddict(options =>
+            {
+                // Register the Entity Framework stores.
+                options.AddEntityFrameworkCoreStores<ApplicationDbContext>();
+
+                // Register the ASP.NET Core MVC binder used by OpenIddict.
+                // Note: if you don't call this method, you won't be able to
+                // bind OpenIdConnectRequest or OpenIdConnectResponse parameters.
+                options.AddMvcBinders();
+
+                // Enable the authorization, logout, token and userinfo endpoints.
+                options.EnableAuthorizationEndpoint("/connect/authorize")
+                       .EnableLogoutEndpoint("/connect/logout")
+                       .EnableTokenEndpoint("/connect/token")
+                       .EnableUserinfoEndpoint("/api/userinfo");
+
+                // Note: the Mvc.Client sample only uses the code flow and the password flow, but you
+                // can enable the other flows if you need to support implicit or client credentials.
+                options.AllowAuthorizationCodeFlow()
+                       .AllowPasswordFlow()
+                       .AllowRefreshTokenFlow();
+
+                // Mark the "profile" scope as a supported scope in the discovery document.
+                options.RegisterScopes(OpenIdConnectConstants.Scopes.Profile);
+
+                // Make the "client_id" parameter mandatory when sending a token request.
+                options.RequireClientIdentification();
+
+                // When request caching is enabled, authorization and logout requests
+                // are stored in the distributed cache by OpenIddict and the user agent
+                // is redirected to the same page with a single parameter (request_id).
+                // This allows flowing large OpenID Connect requests even when using
+                // an external authentication provider like Google, Facebook or Twitter.
+                options.EnableRequestCaching();
+
+                // During development, you can disable the HTTPS requirement.
+                options.DisableHttpsRequirement();
+
+                // Note: to use JWT access tokens instead of the default
+                // encrypted format, the following lines are required:
+                //
+                // options.UseJsonWebTokens();
+                // options.AddEphemeralSigningKey();
+            });
 
             services.AddMvc()
             .AddJsonOptions(
@@ -86,7 +146,10 @@ namespace ggcvan
             {
                 c.SwaggerDoc("v1", new Info { Title = "GGC VAN", Version = "v1" });
             });
+
+            services.AddTransient<IEmailSender, EmailSender>();
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDbContext ctx)
