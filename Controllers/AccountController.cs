289e6a7f9e14c7382ec3050fa304359169b0dd78
Bryn Beaudry <babeaudry@gmail.com>
2017-12-01 00:03:29 -0800

    Made changes to account controller

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -97,22 +97,25 @@ namespace ggcvan.Controllers
 
         [AllowAnonymous]
         [HttpPost("/api/register")]
-        public IActionResult ApiRegister([FromBody]LoginViewModel lmv)
+        public async Task<IActionResult> ApiRegisterAsync([FromBody]LoginViewModel lmv)
         {
-            try 
-            {
                 // save 
-                var identityResult = _userManager.CreateAsync(new ApplicationUser(){ Email = lmv.Email, UserName = lmv.Email}, lmv.Password);
-                if(identityResult.IsFaulted){
-                    throw new Exception("Your request is faulted.");
+                var result = await _userManager.CreateAsync(new ApplicationUser(){ Email = lmv.Email, UserName = lmv.Email}, lmv.Password);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+
+                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    //var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
+                    //await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
+
+                    //await _signInManager.SignInAsync(user, isPersistent: false);
+                    //_logger.LogInformation("User created a new account with password.");
+                    return Ok(result);
+                    //return RedirectToLocal(returnUrl);
                 }
-            } 
-            catch(Exception ex)
-            {
-                // return error message if there was an exception
-                return BadRequest(ex.Message);
-            }
-            return Ok(Json("User Created successfully."));
+                AddErrors(result);
+            return BadRequest(result);
         }
 
         
--- README.md
+++
@@ -1,4 +0,0 @@
-# ggcvan\[$(date +%H:%M:%S)\]\ Initial create migration login and sign in work, user in AspNetUsers
- 
- 
- 
