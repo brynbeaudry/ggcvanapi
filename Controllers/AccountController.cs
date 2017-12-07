44efa4d1931eb6f5eacba90fb8ae32f64c3cdd2e
Bryn Beaudry <babeaudry@gmail.com>
2017-12-06 21:18:48 -0800

    Added new Model, modified app settings to include api key, and make a second call to a a google user endpoint ater verifying the token. Works with the ios Client on localhost

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -60,40 +60,6 @@ namespace ggcvan.Controllers
             return View();
         }
 
-        /*
-        [HttpPost]
-        [AllowAnonymous]
-        [Route("api/authentication/FacebookLogin")]
-        //will have o make Facebook account object 
-        public async Task<IActionResult> FacebookLogin([FromBody] FacebookToken facebookToken)
-        {
-            //check token
-            var httpClient = new HttpClient { BaseAddress = new Uri("") };
-            var response = await httpClient.GetAsync($"me?access_token={facebookToken.Token}&fields=id,name,email,first_name,last_name,age_range,birthday,gender,locale,picture");
-            if (!response.IsSuccessStatusCode) return BadRequest();
-            var result = await response.Content.ReadAsStringAsync();
-            //will have to make facebook account
-           // var facebookAccount = JsonConvert.DeserializeObject<FacebookAccount>(result);
-
-            //register if required
-            //may need to make facebook users DB set
-            /*
-            var facebookUser = _userManager.FindByLoginAsync("Facebook", );
-            if (facebookUser == null)
-            {
-                var user = new ApplicationUser { UserName = facebookAccount.Name, Email = facebookAccount.Email };
-                var result2 = await _userManager.CreateAsync(user);
-                if (!result2.Succeeded) return BadRequest();
-                facebookUser = new FacebookUser { Id = facebookAccount.Id, UserId = user.Id };
-                _context.FacebookUsers.Add(facebookUser);
-                _context.SaveChanges();
-            }
-
-            //send bearer token
-            return Ok(GetToken(facebookUser.UserId));
-            }
-          
-    */  
 
         [AllowAnonymous]
         [HttpPost("/api/register")]
